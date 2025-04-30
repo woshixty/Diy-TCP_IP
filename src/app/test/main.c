463f79c17c954c02f740c32306891ed2c49642a7
xiety <qyyzxty@icloud.com>
2025-04-30 22:12:32 +0800

    3-2  向网络上发送数据包

--- src/app/test/main.c
+++ src/app/test/main.c
@@ -11,12 +11,30 @@
 #include <stdio.h>
 #include "sys_plat.h"
 
+#define SYS_PLAT_WINDOWS 1
+
 int main (void) {
 	// 以下是测试代码，可以删掉
 	// 打开物理网卡，设置好硬件地址
 	static const uint8_t netdev0_hwaddr[] = { 0x00, 0x50, 0x56, 0xc0, 0x00, 0x11 };
 	pcap_t* pcap = pcap_device_open("192.168.74.1", netdev0_hwaddr);
-	sys_sleep(1000);
+	while (pcap)
+	{
+		static uint8_t buffer[1024];
+		memset(buffer, 0, sizeof(buffer));
+
+		static int counter = 0;
+		plat_printf("begin test: %d\n", counter++);
+		
+		if(pcap_inject(pcap, buffer, sizeof(buffer)) == -1)
+		{
+			plat_printf("pcap send: send packet failed %s\n", pcap_geterr(pcap));
+			break;
+		}
+
+		sys_sleep(10);
+	}
+	
 
 	printf("Hello, world");
 	return 0;
--- src/plat/sys_plat.c
+++ src/plat/sys_plat.c
@@ -10,133 +10,133 @@
  */
 #include "sys_plat.h"
 
-#if defined(SYS_PLAT_X86OS)
-#include "dev/time.h"
-#include "os_cfg.h"
-#include "dbg.h"
-#include "sys.h"
-#include "mblock.h"
-#include "cpu/irq.h"
-#include "core/task.h"
+// #if defined(SYS_PLAT_X86OS)
+// #include "dev/time.h"
+// #include "os_cfg.h"
+// #include "dbg.h"
+// #include "sys.h"
+// #include "mblock.h"
+// #include "cpu/irq.h"
+// #include "core/task.h"
 
-#define NET_TASK_NR                 3           // 如果收发均有线程，至少3个。否则1个
-#define NET_SEM_NR                  100         // 信号量数量
-#define NET_MUTEX_NR                100         // 互斥锁数量
+// #define NET_TASK_NR                 3           // 如果收发均有线程，至少3个。否则1个
+// #define NET_SEM_NR                  100         // 信号量数量
+// #define NET_MUTEX_NR                100         // 互斥锁数量
 
-typedef struct _net_task_t {
-    task_t task;
-    uint32_t stack[10*1024];
-}net_task_t;
+// typedef struct _net_task_t {
+//     task_t task;
+//     uint32_t stack[10*1024];
+// }net_task_t;
 
-static net_task_t task_tbl[NET_TASK_NR];
-static mblock_t task_mblock;
-static sem_t sem_tbl[NET_SEM_NR];
-static mblock_t sem_mblock;
-static mutex_t mutex_tbl[NET_MUTEX_NR];
-static mblock_t mutex_mblock;
+// static net_task_t task_tbl[NET_TASK_NR];
+// static mblock_t task_mblock;
+// static sem_t sem_tbl[NET_SEM_NR];
+// static mblock_t sem_mblock;
+// static mutex_t mutex_tbl[NET_MUTEX_NR];
+// static mblock_t mutex_mblock;
 
-void sys_time_curr (net_time_t * time) {
-    *time = sys_get_ticks();
-}
+// void sys_time_curr (net_time_t * time) {
+//     *time = sys_get_ticks();
+// }
 
-int sys_time_goes (net_time_t * pre) {
-    // 获取当前时间
-    net_time_t curr = sys_get_ticks();
+// int sys_time_goes (net_time_t * pre) {
+//     // 获取当前时间
+//     net_time_t curr = sys_get_ticks();
 
-    // 记录过去了多少毫秒
-   int diff_ms = (curr - *pre) * OS_TICK_MS;
+//     // 记录过去了多少毫秒
+//    int diff_ms = (curr - *pre) * OS_TICK_MS;
 
-    // 记录下这次调用的时间
-    *pre  = curr;
-    return diff_ms;    
-}
+//     // 记录下这次调用的时间
+//     *pre  = curr;
+//     return diff_ms;    
+// }
 
-// 计数信号量相关：由具体平台实现
-sys_sem_t sys_sem_create(int init_count) {
-    sys_sem_t sem = (sys_sem_t)mblock_alloc(&sem_mblock, -1);
-    if (sem != SYS_SEM_INVALID) {
-       sem_init(sem, init_count);
-    }
-    return sem;
-}
+// // 计数信号量相关：由具体平台实现
+// sys_sem_t sys_sem_create(int init_count) {
+//     sys_sem_t sem = (sys_sem_t)mblock_alloc(&sem_mblock, -1);
+//     if (sem != SYS_SEM_INVALID) {
+//        sem_init(sem, init_count);
+//     }
+//     return sem;
+// }
 
-void sys_sem_free(sys_sem_t sem) {
-    mblock_free(&sem_mblock, sem);
-}
+// void sys_sem_free(sys_sem_t sem) {
+//     mblock_free(&sem_mblock, sem);
+// }
 
-int sys_sem_wait(sys_sem_t sem, uint32_t ms) {
-    return sem_wait_tmo(sem, ms);
-}
+// int sys_sem_wait(sys_sem_t sem, uint32_t ms) {
+//     return sem_wait_tmo(sem, ms);
+// }
 
-void sys_sem_notify(sys_sem_t sem) {
-    sem_notify(sem);
-}
+// void sys_sem_notify(sys_sem_t sem) {
+//     sem_notify(sem);
+// }
 
-// 互斥信号量：由具体平台实现
-sys_mutex_t sys_mutex_create(void) {
-    sys_mutex_t m = (sys_mutex_t)mblock_alloc(&mutex_mblock, -1);
-    if (m != SYS_MUTEx_INVALID) {
-        mutex_init(m);
-    }
-    return m;
-}
+// // 互斥信号量：由具体平台实现
+// sys_mutex_t sys_mutex_create(void) {
+//     sys_mutex_t m = (sys_mutex_t)mblock_alloc(&mutex_mblock, -1);
+//     if (m != SYS_MUTEx_INVALID) {
+//         mutex_init(m);
+//     }
+//     return m;
+// }
 
-void sys_mutex_free(sys_mutex_t mutex) {
-    mblock_free(&mutex_mblock, mutex);
-}
+// void sys_mutex_free(sys_mutex_t mutex) {
+//     mblock_free(&mutex_mblock, mutex);
+// }
 
-void sys_mutex_lock(sys_mutex_t mutex) {
-    mutex_lock(mutex);
-}
+// void sys_mutex_lock(sys_mutex_t mutex) {
+//     mutex_lock(mutex);
+// }
 
-void sys_mutex_unlock(sys_mutex_t mutex) {
-    mutex_unlock(mutex);
-}
+// void sys_mutex_unlock(sys_mutex_t mutex) {
+//     mutex_unlock(mutex);
+// }
 
-sys_intlocker_t sys_intlocker_lock(void) {
-    return irq_enter_protection();
-}
+// sys_intlocker_t sys_intlocker_lock(void) {
+//     return irq_enter_protection();
+// }
 
-void sys_intlocker_unlock(sys_intlocker_t locker) {
-    irq_leave_protection(locker);
-}
+// void sys_intlocker_unlock(sys_intlocker_t locker) {
+//     irq_leave_protection(locker);
+// }
 
-// 线程相关：由具体平台实现
-sys_thread_t sys_thread_create(sys_thread_func_t entry, void* arg) {
-    net_task_t * task = (net_task_t *)mblock_alloc(&task_mblock, -1);
+// // 线程相关：由具体平台实现
+// sys_thread_t sys_thread_create(sys_thread_func_t entry, void* arg) {
+//     net_task_t * task = (net_task_t *)mblock_alloc(&task_mblock, -1);
 
-    // 初始化系统线程
-    int err = task_init(&task->task, "net task", TASK_FLAG_SYSTEM, (uint32_t)entry, (uint32_t)(task->stack + sizeof(task->stack)));
-    if (err < 0) {
-        log_printf("net: create task failed.");
-        return SYS_THREAD_INVALID;
-    }
+//     // 初始化系统线程
+//     int err = task_init(&task->task, "net task", TASK_FLAG_SYSTEM, (uint32_t)entry, (uint32_t)(task->stack + sizeof(task->stack)));
+//     if (err < 0) {
+//         log_printf("net: create task failed.");
+//         return SYS_THREAD_INVALID;
+//     }
 
-    // 注意启动任务
-    task_start(&task->task);
-    return &task->task;
-}
+//     // 注意启动任务
+//     task_start(&task->task);
+//     return &task->task;
+// }
 
-void sys_thread_exit (int error) {
-    // 不实现，加入os内核后，应用层不需要使用该接口
-}
+// void sys_thread_exit (int error) {
+//     // 不实现，加入os内核后，应用层不需要使用该接口
+// }
 
-sys_thread_t sys_thread_self (void) {
-    return task_current();
-}
+// sys_thread_t sys_thread_self (void) {
+//     return task_current();
+// }
 
-void sys_sleep(int ms) {
-    sys_msleep(ms);
-}
+// void sys_sleep(int ms) {
+//     sys_msleep(ms);
+// }
 
-void sys_plat_init(void) {
-    mblock_init(&task_mblock, task_tbl, sizeof(net_task_t), NET_TASK_NR, NLOCKER_NONE);
-    mblock_init(&sem_mblock, sem_tbl, sizeof(sem_t), NET_SEM_NR, NLOCKER_NONE);
-    mblock_init(&mutex_mblock, mutex_tbl, sizeof(mutex_t), NET_MUTEX_NR, NLOCKER_NONE);
-}
+// void sys_plat_init(void) {
+//     mblock_init(&task_mblock, task_tbl, sizeof(net_task_t), NET_TASK_NR, NLOCKER_NONE);
+//     mblock_init(&sem_mblock, sem_tbl, sizeof(sem_t), NET_SEM_NR, NLOCKER_NONE);
+//     mblock_init(&mutex_mblock, mutex_tbl, sizeof(mutex_t), NET_MUTEX_NR, NLOCKER_NONE);
+// }
 	
- // windows
-#elif defined(SYS_PLAT_WINDOWS)
+// windows
+// #elif defined(SYS_PLAT_WINDOWS)
 
 #include <winsock.h>
 #include <tchar.h>
@@ -295,215 +295,215 @@ void sys_sleep(int ms) {
 void sys_plat_init(void) {
 }
 
-#elif defined(SYS_PLAT_LINUX) || defined(SYS_PLAT_MAC)
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <pthread.h>
-#include <semaphore.h>
-#include "pcap.h"
+// #elif defined(SYS_PLAT_LINUX) || defined(SYS_PLAT_MAC)
+// #include <netinet/in.h>
+// #include <arpa/inet.h>
+// #include <pthread.h>
+// #include <semaphore.h>
+// #include "pcap.h"
 
-#include <stdio.h>
-#include <pthread.h>
-#include <string.h>
-#include <unistd.h>
-#include <semaphore.h>
-#include <sys/time.h>
+// #include <stdio.h>
+// #include <pthread.h>
+// #include <string.h>
+// #include <unistd.h>
+// #include <semaphore.h>
+// #include <sys/time.h>
 
-int load_pcap_lib(void) {
-    return 0;
-}
+// int load_pcap_lib(void) {
+//     return 0;
+// }
 
-/**
- * @brief 获取当前时间
- */
-void sys_time_curr (net_time_t * time) {
-    gettimeofday(time, NULL);
-}
+// /**
+//  * @brief 获取当前时间
+//  */
+// void sys_time_curr (net_time_t * time) {
+//     gettimeofday(time, NULL);
+// }
 
-/**
- * @brief 返回当前时间与传入的time之间时间差值, 调用完成之后，time被更新为当前时间
- * 
- * 第一次调用时，返回的时间差值无效
- */
-int sys_time_goes (net_time_t * pre) {
-    // 获取当前时间
-    struct timeval curr;
-    gettimeofday(&curr, NULL);
+// /**
+//  * @brief 返回当前时间与传入的time之间时间差值, 调用完成之后，time被更新为当前时间
+//  * 
+//  * 第一次调用时，返回的时间差值无效
+//  */
+// int sys_time_goes (net_time_t * pre) {
+//     // 获取当前时间
+//     struct timeval curr;
+//     gettimeofday(&curr, NULL);
 
-    // 记录过去了多少毫秒
-    int diff_ms = (curr.tv_sec - pre->tv_sec) * 1000 + (curr.tv_usec - pre->tv_usec) / 1000;
+//     // 记录过去了多少毫秒
+//     int diff_ms = (curr.tv_sec - pre->tv_sec) * 1000 + (curr.tv_usec - pre->tv_usec) / 1000;
 
-    // 记录下这次调用的时间
-    *pre  = curr;
-    return diff_ms;
-}
+//     // 记录下这次调用的时间
+//     *pre  = curr;
+//     return diff_ms;
+// }
 
-sys_sem_t sys_sem_create(int init_count) {
-    sys_sem_t sem = (sys_sem_t)malloc(sizeof(struct _xsys_sem_t));
-    if (!sem) {
-        return (sys_sem_t)0;
-    }
+// sys_sem_t sys_sem_create(int init_count) {
+//     sys_sem_t sem = (sys_sem_t)malloc(sizeof(struct _xsys_sem_t));
+//     if (!sem) {
+//         return (sys_sem_t)0;
+//     }
 
-    sem->count = init_count;
+//     sem->count = init_count;
 
-    int err = pthread_cond_init(&(sem->cond), NULL);
-    if (err) {
-        return (sys_sem_t)0;
-    }
+//     int err = pthread_cond_init(&(sem->cond), NULL);
+//     if (err) {
+//         return (sys_sem_t)0;
+//     }
 
-    err = pthread_mutex_init(&(sem->locker), NULL);
-    if (err) {
-        return (sys_sem_t)0;
-    }
+//     err = pthread_mutex_init(&(sem->locker), NULL);
+//     if (err) {
+//         return (sys_sem_t)0;
+//     }
 
-    return sem;
-}
+//     return sem;
+// }
 
-/**
- * 释放掉信号量
- */
-void sys_sem_free(sys_sem_t sem) {
-    pthread_cond_destroy(&(sem->cond));
-    pthread_mutex_destroy(&(sem->locker));
-    free(sem);
-}
+// /**
+//  * 释放掉信号量
+//  */
+// void sys_sem_free(sys_sem_t sem) {
+//     pthread_cond_destroy(&(sem->cond));
+//     pthread_mutex_destroy(&(sem->locker));
+//     free(sem);
+// }
 
-/**
- * 等待信号量
- * @param sem 等待的信号量
- * @param tmo 等待的超时时间
- */
-int sys_sem_wait(sys_sem_t sem, uint32_t tmo_ms) {
-    pthread_mutex_lock(&(sem->locker));
+// /**
+//  * 等待信号量
+//  * @param sem 等待的信号量
+//  * @param tmo 等待的超时时间
+//  */
+// int sys_sem_wait(sys_sem_t sem, uint32_t tmo_ms) {
+//     pthread_mutex_lock(&(sem->locker));
 
-    if (sem->count <= 0) {
-        int ret;
+//     if (sem->count <= 0) {
+//         int ret;
 
-        if (tmo_ms > 0) {
-            struct timespec ts;
-            ts.tv_nsec = (tmo_ms % 1000) * 1000000L;
-            ts.tv_sec = time(NULL) + tmo_ms / 1000;
-            ret = pthread_cond_timedwait(&sem->cond, &sem->locker, &ts);
-            if (ret == ETIMEDOUT) {
-                pthread_mutex_unlock(&(sem->locker));
-                return -1;
-            }
-        } else {
-            ret = pthread_cond_wait(&sem->cond, &sem->locker);
-            if (ret < 0) {
-                pthread_mutex_unlock(&(sem->locker));
-                return -1;
-            }
-        }
-    }
+//         if (tmo_ms > 0) {
+//             struct timespec ts;
+//             ts.tv_nsec = (tmo_ms % 1000) * 1000000L;
+//             ts.tv_sec = time(NULL) + tmo_ms / 1000;
+//             ret = pthread_cond_timedwait(&sem->cond, &sem->locker, &ts);
+//             if (ret == ETIMEDOUT) {
+//                 pthread_mutex_unlock(&(sem->locker));
+//                 return -1;
+//             }
+//         } else {
+//             ret = pthread_cond_wait(&sem->cond, &sem->locker);
+//             if (ret < 0) {
+//                 pthread_mutex_unlock(&(sem->locker));
+//                 return -1;
+//             }
+//         }
+//     }
 
-    sem->count--;
-    pthread_mutex_unlock(&(sem->locker));
-    return 0;
-}
+//     sem->count--;
+//     pthread_mutex_unlock(&(sem->locker));
+//     return 0;
+// }
 
-/**
- * 通知信号量
- * @param sem 待通知的信号量
- */
-void sys_sem_notify(sys_sem_t sem) {
-    pthread_mutex_lock(&(sem->locker));
+// /**
+//  * 通知信号量
+//  * @param sem 待通知的信号量
+//  */
+// void sys_sem_notify(sys_sem_t sem) {
+//     pthread_mutex_lock(&(sem->locker));
 
-    sem->count++;
+//     sem->count++;
 
-    // 通知线程，有新的资源可用
-    pthread_cond_signal(&(sem->cond));
+//     // 通知线程，有新的资源可用
+//     pthread_cond_signal(&(sem->cond));
 
-    pthread_mutex_unlock(&(sem->locker));
-}
+//     pthread_mutex_unlock(&(sem->locker));
+// }
 
-/**
- * 创建一个线程
- * @param entry 线程的入口函数
- * @param arg 传递给线程的参数
- * @param prio 优先级
- * @param stack_size 堆栈大小
- */
-sys_thread_t sys_thread_create(void (*entry)(void * arg), void* arg) {
-    pthread_t pthread;
+// /**
+//  * 创建一个线程
+//  * @param entry 线程的入口函数
+//  * @param arg 传递给线程的参数
+//  * @param prio 优先级
+//  * @param stack_size 堆栈大小
+//  */
+// sys_thread_t sys_thread_create(void (*entry)(void * arg), void* arg) {
+//     pthread_t pthread;
 
-    int err = pthread_create(&pthread,
-                         NULL,
-                         (void* (*)(void * arg))entry,
-                         arg);
-    if (err) {
-        return (pthread_t)0;
-    }
+//     int err = pthread_create(&pthread,
+//                          NULL,
+//                          (void* (*)(void * arg))entry,
+//                          arg);
+//     if (err) {
+//         return (pthread_t)0;
+//     }
 
-    return pthread;
-}
+//     return pthread;
+// }
 
-/**
- * 销毁线程
- */
-void sys_thread_del_self() {
-    pthread_exit(NULL);
-}
+// /**
+//  * 销毁线程
+//  */
+// void sys_thread_del_self() {
+//     pthread_exit(NULL);
+// }
 
-/**
- * @brief 简单的延时，以毫秒为单位
- */
-void sys_sleep(int ms) {
-    usleep(1000 * ms);
-}
+// /**
+//  * @brief 简单的延时，以毫秒为单位
+//  */
+// void sys_sleep(int ms) {
+//     usleep(1000 * ms);
+// }
 
-/**
- * 创建线程互斥锁
- * @return 创建的互斥信号量
- */
-sys_mutex_t sys_mutex_create(void) {
-    sys_mutex_t mutex = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
+// /**
+//  * 创建线程互斥锁
+//  * @return 创建的互斥信号量
+//  */
+// sys_mutex_t sys_mutex_create(void) {
+//     sys_mutex_t mutex = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
 
-    int err = pthread_mutex_init(mutex, NULL);
-    if (err < 0) {
-        return (sys_mutex_t)0;
-    }
-    return mutex;
-}
+//     int err = pthread_mutex_init(mutex, NULL);
+//     if (err < 0) {
+//         return (sys_mutex_t)0;
+//     }
+//     return mutex;
+// }
 
-/**
- * 释放互斥信号量
- * @param mutex
- */
-void sys_mutex_free(sys_mutex_t locker) {
-    pthread_mutex_destroy(locker);
-    free(locker);
-}
+// /**
+//  * 释放互斥信号量
+//  * @param mutex
+//  */
+// void sys_mutex_free(sys_mutex_t locker) {
+//     pthread_mutex_destroy(locker);
+//     free(locker);
+// }
 
-/**
- * 锁定线程互斥锁
- * @param mutex 待锁定的互斥信号量
- */
-void sys_mutex_lock(sys_mutex_t locker) {
-    pthread_mutex_lock(locker);
-}
+// /**
+//  * 锁定线程互斥锁
+//  * @param mutex 待锁定的互斥信号量
+//  */
+// void sys_mutex_lock(sys_mutex_t locker) {
+//     pthread_mutex_lock(locker);
+// }
 
-/**
- * 释放线程互斥锁
- * @param mutex 待释放的互斥信号量
- */
-void sys_mutex_unlock(sys_mutex_t locker) {
-    pthread_mutex_unlock(locker);
-}
+// /**
+//  * 释放线程互斥锁
+//  * @param mutex 待释放的互斥信号量
+//  */
+// void sys_mutex_unlock(sys_mutex_t locker) {
+//     pthread_mutex_unlock(locker);
+// }
 
 
-void sys_thread_exit (int error) {
-    // 不实现，加入os内核后，应用层不需要使用该接口
-}
+// void sys_thread_exit (int error) {
+//     // 不实现，加入os内核后，应用层不需要使用该接口
+// }
 
-sys_thread_t sys_thread_self (void) {
-    return pthread_self();
-}
+// sys_thread_t sys_thread_self (void) {
+//     return pthread_self();
+// }
 
-void sys_plat_init(void) {
-}
+// void sys_plat_init(void) {
+// }
 
-#endif
+// #endif
 
 #if defined(NET_DRIVER_PCAP)
 #include <pcap.h>
