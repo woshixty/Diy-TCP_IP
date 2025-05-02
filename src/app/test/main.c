50611c2ab1673296e34c6c3236675870b8921c70
xiety <qyyzxty@icloud.com>
2025-05-02 22:28:36 +0800

    3-10 线程间的资源访问控制实例

--- src/app/test/main.c
+++ src/app/test/main.c
@@ -4,8 +4,13 @@
 #define SYS_PLAT_WINDOWS 1
 
 static sys_sem_t sem;
-static int count;
 static sys_mutex_t mutex;
+static int count;
+
+static char buffer[100];
+static sys_sem_t read_sem;      // 读信号量
+static sys_sem_t write_sem;     // 写信号量
+static int write_idx, read_idx, total;  // 写索引和读索引，总计数
 
 void thread1_entry (void* arg) {
     // count的值大一些，效果才显著
@@ -15,6 +20,32 @@ void thread1_entry (void* arg) {
         sys_mutex_unlock(mutex);
     }
     plat_printf("thread 1: count = %d\n", count);
+
+    // 不断读取数据
+    for (int i = 0; i < 2 * sizeof(buffer); i++) {
+        // 等待有数据，即读信号量中的数据资源计数不为0
+        sys_sem_wait(read_sem, 0);
+
+        // 取出一个数据
+        unsigned char data = buffer[read_idx++];
+        if (read_idx >= sizeof(buffer)) {
+            read_idx = 0;
+        } 
+
+        // 显示当前读的内容
+        plat_printf("thread 1 read data=%d\n", data);
+
+        // 调整总计数
+        sys_mutex_lock(mutex);
+        total--;
+        sys_mutex_unlock(mutex);
+
+        // 发信号给写线程，通知可以进行写了
+        sys_sem_notify(write_sem);
+
+        // 延时一会儿，让读取的速度慢一些
+        sys_sleep(100);
+    }
 
     while (1) {
         plat_printf("this is thread 1: %s\n", (char *)arg);
@@ -32,6 +63,29 @@ void thread2_entry(void* arg) {
     }
     plat_printf("thread 2: count = %d\n", count);
 
+    // 连续写200次，由于中间没有printf、延时，所以写的速度是比较快的
+    for (int i = 0; i < 2 * sizeof(buffer); i++) {
+        // 写之前要等有可用的写空间，看看写信号量中空闲资源的数量不为0
+        sys_sem_wait(write_sem, 0);
+
+        // 不为0则将数据写入
+        buffer[write_idx++] = i;
+        if (write_idx >= sizeof(buffer)) {
+            write_idx = 0;
+        } 
+        // 显示当前写的内容，方便观察
+        plat_printf("thread 2 write data=%d\n", i);
+
+        // 上锁，保护total的资源计数
+        sys_mutex_lock(mutex);
+        total++;
+        sys_mutex_unlock(mutex);
+
+        // 通知另一个线程可以进行读取，但具体什么时候读则不确定
+        sys_sem_notify(read_sem);
+    }
+    sys_sleep(100000);      // 加点延时，避免后面的代码干扰
+
     while (1) {
         sys_sem_wait(sem, 0);
         plat_printf("this is thread 2: %s\n", (char *)arg);
@@ -42,10 +96,15 @@ int main (void) {
     // 注意放在线程的创建的前面，以便线程运行前就准备好
     sem = sys_sem_create(0);
     mutex = sys_mutex_create();
-    plat_printf("thread main: count = %d\n", count);
+
+    // 创建读写信号量
+    read_sem = sys_sem_create(0);
+    write_sem = sys_sem_create(sizeof(buffer));
 
     sys_thread_create(thread1_entry, "AAAA");
     sys_thread_create(thread2_entry, "BBBB");
+
+    while (1) {}
 	
 	pcap_t* pcap = pcap_device_open(netdev0_phy_ip, netdev0_hwaddr);
 	while (pcap)
