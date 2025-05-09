08cbcd3f2d4cca3707cc4af28da75b82e2e55fcb
xiety <qyyzxty@icloud.com>
2025-05-09 22:43:45 +0800

    5-2 给调试信息增加颜色

--- src/net/net/dbg.h
+++ src/net/net/dbg.h
@@ -1,6 +1,10 @@
 #ifndef DBG_H
 #define DBG_H
 
+#define DBG_STYLE_ERROR         "\033[31m"
+#define DBG_STYLE_WARNING       "\033[33m"
+#define DBG_STYLE_RESET         "\033[0m"
+
 void dbg_print(const char* file, const char* func, int line, const char* fmt, ...);
 
 #define dbg_info(fmt, ...) dbg_print(__FILE__, __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
