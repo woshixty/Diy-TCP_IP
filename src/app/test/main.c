68dd5aa3cf1e8ab75c5f91c60b6cc0110ea0273e
xiety <qyyzxty@icloud.com>
2025-05-30 20:23:58 +0800

    9-17 定位数据包

--- src/app/test/main.c
+++ src/app/test/main.c
@@ -273,6 +273,22 @@ void pktbuf_test()
         plat_printf("not equal");
         return;
     }
+
+    plat_memset(read_temp, 0, sizeof(read_temp));
+    pktbuf_seek(buf, 18 * 2);
+    pktbuf_read(buf, (uint8_t*)read_temp, 56);
+    if(plat_memcmp(temp + 18, read_temp, 56) != 0) {
+        plat_printf("not equal");
+        return;
+    }
+
+    plat_memset(read_temp, 0, sizeof(read_temp));
+    pktbuf_seek(buf, 85 * 2);
+    pktbuf_read(buf, (uint8_t*)read_temp, 256);
+    if(plat_memcmp(temp + 85, read_temp, 256) != 0) {
+        plat_printf("not equal");
+        return;
+    }
 }
 
 /**
--- src/net/net/pktbuf.h
+++ src/net/net/pktbuf.h
@@ -54,7 +54,8 @@ net_err_t pktbuf_resize(pktbuf_t* buf, int to_size);
 net_err_t pktbuf_join(pktbuf_t* dest, pktbuf_t* src);
 net_err_t pktbuf_set_cont(pktbuf_t* buf, int size);
 void pktbuf_reset_acc(pktbuf_t* buf);
-int pktbuf_write(pktbuf_t* buf, uint8_t* src, int size);
-int pktbuf_read(pktbuf_t* buf, uint8_t* dest, int size);
+net_err_t pktbuf_write(pktbuf_t* buf, uint8_t* src, int size);
+net_err_t pktbuf_read(pktbuf_t* buf, uint8_t* dest, int size);
+net_err_t pktbuf_seek(pktbuf_t* buf, int offset);
 
 #endif
